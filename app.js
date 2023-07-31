f4b583726eb63ce83cc9934b2831a2df4c294fb5
rohail <rupani.rohail@gmail.com>
2023-07-31 15:02:54 +0500

    message

--- app.js
+++ app.js
@@ -1,244 +1,65 @@
+function setcitiesweather() {
+    let cities = ["karachi", "lahore", "melbourne", "sydney", "london", "washington"]
 
-function setweatherkar() {
-    let data
-    fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=karachi&appid=4fb3ea530ea91d0e1c04f26d9b759862&units=metric`
-    )
-        .then((res) => res.json())
-        .then((res) => {
-            data = res
-            console.log(data)
-            let weather_img_kar = document.getElementById("weather-img-kar")
-            let karTemp = document.getElementById("karTemp")
-            karTemp.innerHTML = data.main.temp + "°C"
+    for (i = 0; i < cities.length; i++) {
+        let displaycity = cities[i]
 
-            if (data.weather[0].main === "Rain" || data.weather[0].main === "Drizzle") {
-                weather_img_kar.src = "./images/rainy-day.png"
-            }
-            else if (data.weather[0].main === "Clear") {
-                weather_img_kar.src = "./images/clear-sky.png";
-            }
-            else if (data.weather[0].main === "Haze" || data.weather[0].main === "Clouds") {
-                weather_img_kar.src = "./images/cloudy.png";
-            }
-            else if (data.weather[0].main === "Dust" || data.weather[0].main === "Sand") {
-                weather_img_kar.src = "./images/sand.png";
-            }
-            else if (data.weather[0].main === "Mist" || data.weather[0].main === "Fog" || data.weather[0].main === "Smoke") {
-                weather_img_kar.src = "./images/smoke.png";
-            }
-            else if (data.weather[0].main === "Snow") {
-                weather_img_kar.src = "./images/snowflake.png";
-            }
-            else if (data.weather[0].main === "Thunderstorm") {
-                weather_img_kar.src = "./images/thunderstorm.png";
-            }
+        let data
+        fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${displaycity}&appid=4fb3ea530ea91d0e1c04f26d9b759862&units=metric`
+        )
+            .then((res) => res.json())
+            .then((res) => {
+                data = res
+                console.log(data)
+                if (data.weather[0].main === "Rain" || data.weather[0].main === "Drizzle") {
+                    weather_img_cities = "./images/rainy-day.png"
+                }
+                else if (data.weather[0].main === "Clear") {
+                    weather_img_cities = "./images/clear-sky.png";
+                }
+                else if (data.weather[0].main === "Haze" || data.weather[0].main === "Clouds") {
+                    weather_img_cities = "./images/cloudy.png";
+                }
+                else if (data.weather[0].main === "Dust" || data.weather[0].main === "Sand") {
+                    weather_img_cities = "./images/sand.png";
+                }
+                else if (data.weather[0].main === "Mist" || data.weather[0].main === "Fog" || data.weather[0].main === "Smoke") {
+                    weather_img_cities = "./images/smoke.png";
+                }
+                else if (data.weather[0].main === "Snow") {
+                    weather_img_cities = "./images/snowflake.png";
+                }
+                else if (data.weather[0].main === "Thunderstorm") {
+                    weather_img_cities = "./images/thunderstorm.png";
+                }
 
-        })
+                let showcities = document.getElementById("showcities")
+                // let mycity = document.getElementById("mycity")
+                let citiesTemp = data.main.temp + "°C"
+                showcities.innerHTML += `
+            <div class="col-md-2 col-lg-2 col-sm-12 col-6">
+            <div class=" text-center rounded-2" id="setcities">
+                <h6 class="pt-3">${displaycity}</h6>
+                <img src="${weather_img_cities}" alt="" id="" class="img-fluid pb-2 w-25">
+                <p  class=" pb-2">${citiesTemp}</p>
+            </div>
+        </div>
+            `
+            })
+    }
+    // console.log(cities)
 }
-setweatherkar()
-
-function setweatherlah() {
-    let data
-    fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=lahore&appid=4fb3ea530ea91d0e1c04f26d9b759862&units=metric`
-    )
-        .then((res) => res.json())
-        .then((res) => {
-            data = res
-            console.log(data)
-            let weather_img_lah = document.getElementById("weather-img-lah")
-            let lahoreTemp = document.getElementById("lahoreTemp")
-            lahoreTemp.innerHTML = data.main.temp + "°C"
-
-            if (data.weather[0].main === "Rain" || data.weather[0].main === "Drizzle") {
-                weather_img_lah.src = "./images/rainy-day.png"
-            }
-            else if (data.weather[0].main === "Clear") {
-                weather_img_lah.src = "./images/clear-sky.png";
-            }
-            else if (data.weather[0].main === "Haze" || data.weather[0].main === "Clouds") {
-                weather_img_lah.src = "./images/cloudy.png";
-            }
-            else if (data.weather[0].main === "Dust" || data.weather[0].main === "Sand") {
-                weather_img_lah.src = "./images/sand.png";
-            }
-            else if (data.weather[0].main === "Mist" || data.weather[0].main === "Fog" || data.weather[0].main === "Smoke") {
-                weather_img_lah.src = "./images/smoke.png";
-            }
-            else if (data.weather[0].main === "Snow") {
-                weather_img_lah.src = "./images/snowflake.png";
-            }
-            else if (data.weather[0].main === "Thunderstorm") {
-                weather_img_lah.src = "./images/thunderstorm.png";
-            }
-
-        })
-}
-setweatherlah()
-
-function setweathermel() {
-    let data
-    fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=Melbourne&appid=4fb3ea530ea91d0e1c04f26d9b759862&units=metric`
-    )
-        .then((res) => res.json())
-        .then((res) => {
-            data = res
-            console.log(data)
-            let weather_img_mel = document.getElementById("weather-img-mel")
-            let MelTemp = document.getElementById("MelTemp")
-            MelTemp.innerHTML = data.main.temp + "°C"
-
-            if (data.weather[0].main === "Rain" || data.weather[0].main === "Drizzle") {
-                weather_img_mel.src = "./images/rainy-day.png"
-            }
-            else if (data.weather[0].main === "Clear") {
-                weather_img_mel.src = "./images/clear-sky.png";
-            }
-            else if (data.weather[0].main === "Haze" || data.weather[0].main === "Clouds") {
-                weather_img_mel.src = "./images/cloudy.png";
-            }
-            else if (data.weather[0].main === "Dust" || data.weather[0].main === "Sand") {
-                weather_img_mel.src = "./images/sand.png";
-            }
-            else if (data.weather[0].main === "Mist" || data.weather[0].main === "Fog" || data.weather[0].main === "Smoke") {
-                weather_img_mel.src = "./images/smoke.png";
-            }
-            else if (data.weather[0].main === "Snow") {
-                weather_img_mel.src = "./images/snowflake.png";
-            }
-            else if (data.weather[0].main === "Thunderstorm") {
-                weather_img_mel.src = "./images/thunderstorm.png";
-            }
-
-        })
-}
-setweathermel()
-
-function setweatherLondon() {
-    let data
-    fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=London&appid=4fb3ea530ea91d0e1c04f26d9b759862&units=metric`
-    )
-        .then((res) => res.json())
-        .then((res) => {
-            data = res
-            console.log(data)
-            let weather_img_lon = document.getElementById("weather-img-lon")
-            let londonTemp = document.getElementById("londonTemp")
-            londonTemp.innerHTML = data.main.temp + "°C"
-
-            if (data.weather[0].main === "Rain" || data.weather[0].main === "Drizzle") {
-                weather_img_lon.src = "./images/rainy-day.png"
-            }
-            else if (data.weather[0].main === "Clear") {
-                weather_img_lon.src = "./images/clear-sky.png";
-            }
-            else if (data.weather[0].main === "Haze" || data.weather[0].main === "Clouds") {
-                weather_img_lon.src = "./images/cloudy.png";
-            }
-            else if (data.weather[0].main === "Dust" || data.weather[0].main === "Sand") {
-                weather_img_lon.src = "./images/sand.png";
-            }
-            else if (data.weather[0].main === "Mist" || data.weather[0].main === "Fog" || data.weather[0].main === "Smoke") {
-                weather_img_lon.src = "./images/smoke.png";
-            }
-            else if (data.weather[0].main === "Snow") {
-                weather_img_lon.src = "./images/snowflake.png";
-            }
-            else if (data.weather[0].main === "Thunderstorm") {
-                weather_img_lon.src = "./images/thunderstorm.png";
-            }
-
-        })
-}
-setweatherLondon()
-
-function setweatherWST() {
-    let data
-    fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=washington&appid=4fb3ea530ea91d0e1c04f26d9b759862&units=metric`
-    )
-        .then((res) => res.json())
-        .then((res) => {
-            data = res
-            console.log(data)
-            let weather_img_wst = document.getElementById("weather-img-wst")
-            let wstTemp = document.getElementById("wstTemp")
-            wstTemp.innerHTML = data.main.temp + "°C"
-
-            if (data.weather[0].main === "Rain" || data.weather[0].main === "Drizzle") {
-                weather_img_wst.src = "./images/rainy-day.png"
-            }
-            else if (data.weather[0].main === "Clear") {
-                weather_img_wst.src = "./images/clear-sky.png";
-            }
-            else if (data.weather[0].main === "Haze" || data.weather[0].main === "Clouds") {
-                weather_img_wst.src = "./images/cloudy.png";
-            }
-            else if (data.weather[0].main === "Dust" || data.weather[0].main === "Sand") {
-                weather_img_wst.src = "./images/sand.png";
-            }
-            else if (data.weather[0].main === "Mist" || data.weather[0].main === "Fog" || data.weather[0].main === "Smoke") {
-                weather_img_wst.src = "./images/smoke.png";
-            }
-            else if (data.weather[0].main === "Snow") {
-                weather_img_wst.src = "./images/snowflake.png";
-            }
-            else if (data.weather[0].main === "Thunderstorm") {
-                weather_img_lon.src = "./images/thunderstorm.png";
-            }
-
-        })
-}
-setweatherWST()
-
-function setweathersydney() {
-    let data
-    fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=sydney&appid=4fb3ea530ea91d0e1c04f26d9b759862&units=metric`
-    )
-        .then((res) => res.json())
-        .then((res) => {
-            data = res
-            console.log(data)
-            let weather_img_sydney = document.getElementById("weather-img-sydney")
-            let sydneyTemp = document.getElementById("sydneyTemp")
-            sydneyTemp.innerHTML = data.main.temp + "°C"
-
-            if (data.weather[0].main === "Rain" || data.weather[0].main === "Drizzle") {
-                weather_img_sydney.src = "./images/rainy-day.png"
-            }
-            else if (data.weather[0].main === "Clear") {
-                weather_img_sydney.src = "./images/clear-sky.png";
-            }
-            else if (data.weather[0].main === "Haze" || data.weather[0].main === "Clouds") {
-                weather_img_sydney.src = "./images/cloudy.png";
-            }
-            else if (data.weather[0].main === "Dust" || data.weather[0].main === "Sand") {
-                weather_img_sydney.src = "./images/sand.png";
-            }
-            else if (data.weather[0].main === "Mist" || data.weather[0].main === "Fog" || data.weather[0].main === "Smoke") {
-                weather_img_sydney.src = "./images/smoke.png";
-            }
-            else if (data.weather[0].main === "Snow") {
-                weather_img_sydney.src = "./images/snowflake.png";
-            }
-            else if (data.weather[0].main === "Thunderstorm") {
-                weather_img_lon.src = "./images/thunderstorm.png";
-            }
-
-        })
-}
-setweathersydney()
+setcitiesweather()
 
 let searchbtn = document.getElementById("searchbtn")
 let searchcity = document.getElementById("searchcity")
+
 function setweather() {
     let forcast = document.getElementById("forcast")
+    let set_image = document.getElementById("set_image")
     forcast.style.display = "block"
-   
+
 
     let data
     fetch(
@@ -250,6 +71,9 @@ function setweather() {
             console.log(data)
             let cityname = document.getElementById("cityname")
             cityname.innerHTML = searchcity.value
+
+            let settemp = data.main.temp + "°C"
+            console.log(settemp)
 
             let temperature = document.getElementById("temperature")
             temperature.innerHTML = data.main.temp + "°C"
@@ -265,94 +89,62 @@ function setweather() {
 
             if (data.weather[0].main === "Rain" || data.weather[0].main === "Drizzle") {
                 weather_image.src = "./images/rainy-day.png"
+                set_image.src = "./images/rainy-day.png"
             }
             else if (data.weather[0].main === "Clear") {
                 weather_image.src = "./images/clear-sky.png";
-            }
-            else if (data.weather[0].main === "Haze" || data.weather[0].main === "Clouds") {
-                weather_image.src = "./images/cloudy.png";
-            }
-            else if (data.weather[0].main === "Dust" || data.weather[0].main === "Sand") {
-                weather_image.src = "./images/sand.png";
-            }
-            else if (data.weather[0].main === "Mist" || data.weather[0].main === "Fog" || data.weather[0].main === "Smoke") {
-                weather_image.src = "./images/smoke.png";
-            }
-            else if (data.weather[0].main === "Snow") {
-                weather_image.src = "./images/snowflake.png";
-            }
-            else if (data.weather[0].main === "Thunderstorm") {
-                weather_image.src = "./images/thunderstorm.png";
-            }
-
-        })
-    
-
-showweather()
-}
-searchbtn.addEventListener("click", setweather)
-
-function showweather(){
-    let displayweather = document.getElementById("displayweather")
-    let set_image = document.getElementById("set_image")
-    let data
-    fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${searchcity.value}&appid=4fb3ea530ea91d0e1c04f26d9b759862&units=metric`
-    )
-        .then((res => res.json()))
-        .then((res) => {
-            data = res
-            console.log(data)
-            let citytemp=data.main.temp
-            console.log(citytemp)
-            if (data.weather[0].main === "Rain" || data.weather[0].main === "Drizzle") {
-                set_image = "./images/rainy-day.png"
-            }
-            else if (data.weather[0].main === "Clear") {
                 set_image = "./images/clear-sky.png";
             }
             else if (data.weather[0].main === "Haze" || data.weather[0].main === "Clouds") {
+                weather_image.src = "./images/cloudy.png";
                 set_image = "./images/cloudy.png";
             }
             else if (data.weather[0].main === "Dust" || data.weather[0].main === "Sand") {
+                weather_image.src = "./images/sand.png";
                 set_image = "./images/sand.png";
             }
             else if (data.weather[0].main === "Mist" || data.weather[0].main === "Fog" || data.weather[0].main === "Smoke") {
+                weather_image.src = "./images/smoke.png";
                 set_image = "./images/smoke.png";
             }
             else if (data.weather[0].main === "Snow") {
+                weather_image.src = "./images/snowflake.png";
                 set_image = "./images/snowflake.png";
             }
             else if (data.weather[0].main === "Thunderstorm") {
-                set_image = "./images/thunderstorm.png"
+                weather_image.src = "./images/thunderstorm.png";
+                set_image = "./images/thunderstorm.png";
+
             }
-
+            let displayweather = document.getElementById("displayweather")
             displayweather.innerHTML += `
-        <div class="col-md-4 col-lg-4 col-sm-6 col-12">
-        <div class=" text-center rounded-2" id="yoursearch" onclick="recentsearch('${searchcity.value}')">
-        <h6 class="pt-3"  >${searchcity.value}</h6>
-        <img src="${set_image}" alt="" id="set_image" class="img-fluid pb-2 w-25">
-        <p  class=" pb-2">${citytemp}</p>
-    </div>      
-          </div>
-         `
+            <div class="col-md-4 col-lg-4 col-sm-6 col-12">
+            <div class=" text-center rounded-2" id="yoursearch" onclick="recentsearch('${searchcity.value}')">
+            <h6 class="pt-3">${searchcity.value}</h6>
+            <img src="${set_image}" alt="" id="set_image" class="img-fluid pb-2 w-25">
+            <p class=" pb-2">${settemp}</p>
+        </div>      
+              </div>
+             `
+        })
         
-})
+       
+   
 }
+searchbtn.addEventListener("click", setweather)
 
-let clear_input=document.getElementById("clear")
-function clear(){
-    
-    searchcity.value=""
+
+let clear_input = document.getElementById("clear")
+function clear() {
+
+    searchcity.value = ""
 }
 clear_input.addEventListener("click", clear)
 
-
-
-function recentsearch(x){
+function recentsearch(x) {
     let forcast = document.getElementById("forcast")
     forcast.style.display = "block"
-   
+
     // console.log(x)
     let data
     fetch(
@@ -400,6 +192,6 @@ function recentsearch(x){
             }
 
         })
-    
+
 }
 
